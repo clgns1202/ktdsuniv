5d0f4de9bce0285c1a7248a6346e862e37e87e2d
Yoonyujin <yyj624@naver.com>
2016-11-30 14:41:34 +0900

    no message

--- src/main/java/com/ktdsuniv/admin/room/dao/RoomDao.java
+++ src/main/java/com/ktdsuniv/admin/room/dao/RoomDao.java
@@ -1,5 +1,13 @@
 package com.ktdsuniv.admin.room.dao;
 
+import java.util.List;
+
+import room.schema.RoomsSchema;
+
 public interface RoomDao {
+
+	public void addRoom(RoomsSchema room);
+
+	public List<RoomsSchema> roomList();
 
 }
